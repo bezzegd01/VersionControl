407cb5b489b6e0dd7a22076c9c5bf788ea126474
Dániel Bezzeg <daniel.bezzeg@stud.uni-corvinus.hu>
2021-12-03 17:10:52 +0100

    Majdnem kesz

---
+++ Programtervezesi_mintak/Programtervezesi_mintak/Entities/CarFactory.cs
@@ -0,0 +1,17 @@
+﻿using Programtervezesi_mintak.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programtervezesi_mintak.Entities
+{
+    class CarFactory : iToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Car();
+        }
+    }
+}
