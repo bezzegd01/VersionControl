6a2d5c09e8994a0939e5562f860e93689f7c9b9d
Dániel Bezzeg <daniel.bezzeg@stud.uni-corvinus.hu>
2021-12-10 18:51:07 +0100

    kesz

---
+++ mikroszimulacio/Entities/BirthProbabilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mikroszimulacio.Entities
+{
+    public class BirthProbabilities
+    {
+        public int Age { get; set; }
+        public int NbrofChildren { get; set; }
+        public double P { get; set; }
+
+    }
+}
