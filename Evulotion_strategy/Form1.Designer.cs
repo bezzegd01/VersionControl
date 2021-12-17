044bdbad78ce798568798a72d9264ec3c6a9c6ec
Dániel Bezzeg <daniel.bezzeg@stud.uni-corvinus.hu>
2021-12-17 17:34:32 +0100

    done

--- Evulotion_strategy/Form1.Designer.cs
+++ Evulotion_strategy/Form1.Designer.cs
@@ -29,13 +29,47 @@ namespace Evulotion_strategy
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.generationLabel = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // generationLabel
+            // 
+            this.generationLabel.AutoSize = true;
+            this.generationLabel.Location = new System.Drawing.Point(361, 389);
+            this.generationLabel.Name = "generationLabel";
+            this.generationLabel.Size = new System.Drawing.Size(66, 13);
+            this.generationLabel.TabIndex = 0;
+            this.generationLabel.Text = "1. generáció";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(364, 405);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.generationLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label generationLabel;
+        private System.Windows.Forms.Button startBtn;
     }
 }
 
